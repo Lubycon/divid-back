dde0f7386e0f0e425dd8d261da01b7a514b47362
Yujin Shin <shinnew320@gmail.com>
2021-05-25 19:27:54 +0900

    로그 삭제

--- ourney/src/main/java/com/lubycon/ourney/common/error/GlobalExceptionHandler.java
+++ ourney/src/main/java/com/lubycon/ourney/common/error/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.lubycon.ourney.common.error;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.persistence.EntityNotFoundException;
 import java.nio.file.AccessDeniedException;
 
-@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -36,7 +34,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error(e.getMessage(), e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
