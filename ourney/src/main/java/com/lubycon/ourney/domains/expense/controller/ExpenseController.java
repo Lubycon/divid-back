d7f84fe51ee2fef9c0c32c62d0f6c85044d57109
Yujin Shin <shinnew320@gmail.com>
2021-05-25 19:24:25 +0900

    feat: 여행 지출 리스트 조회

--- ourney/src/main/java/com/lubycon/ourney/domains/expense/controller/ExpenseController.java
+++ ourney/src/main/java/com/lubycon/ourney/domains/expense/controller/ExpenseController.java
@@ -3,8 +3,10 @@ package com.lubycon.ourney.domains.expense.controller;
 import com.lubycon.ourney.common.ResponseMessages;
 import com.lubycon.ourney.common.config.interceptor.LoginId;
 import com.lubycon.ourney.common.error.SimpleSuccessResponse;
+import com.lubycon.ourney.domains.expense.dto.ExpenseListResponse;
 import com.lubycon.ourney.domains.expense.dto.ExpenseRequest;
 import com.lubycon.ourney.domains.expense.service.ExpenseService;
+import com.lubycon.ourney.domains.trip.service.TripService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,13 +14,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
+
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/expenses")
 @RestController
 public class ExpenseController {
     private final ExpenseService expenseService;
+    private final TripService tripService;
 
     @ApiOperation("지출 내역 조회")
     @GetMapping("")
@@ -26,7 +31,7 @@ public class ExpenseController {
             @LoginId long id,
             @RequestParam UUID tripId,
             @RequestParam Long expenseId
-    ){
+    ) {
         return ResponseEntity.ok(expenseService.getExpense(id, tripId, expenseId));
     }
 
@@ -47,7 +52,7 @@ public class ExpenseController {
             @RequestParam UUID tripId,
             @RequestParam Long expenseId,
             @RequestBody ExpenseRequest expenseRequest
-            ){
+    ) {
         expenseService.updateExpense(id, tripId, expenseId, expenseRequest);
         return ResponseEntity.ok(new SimpleSuccessResponse(ResponseMessages.SUCCESS_UPDATE_EXPENSE));
     }
@@ -57,8 +62,17 @@ public class ExpenseController {
     public ResponseEntity deleteExpense(
             @RequestParam UUID tripId,
             @RequestParam Long expenseId
-    ){
+    ) {
         expenseService.deleteExpense(tripId, expenseId);
         return ResponseEntity.ok(new SimpleSuccessResponse(ResponseMessages.SUCCESS_DELETE_EXPENSE));
+    }
+
+    @ApiOperation("지출 리스트 조회")
+    @GetMapping("/all")
+    public ResponseEntity<List<ExpenseListResponse>> getExpenseList(
+            @LoginId long id,
+            @RequestParam UUID tripId
+    ) {
+        return ResponseEntity.ok(expenseService.getExpenseList(id, tripId));
     }
 }
