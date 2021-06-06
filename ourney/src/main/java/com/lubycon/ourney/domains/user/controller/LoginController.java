cf090849701bd8ac02633d0ca4d60ba1825c8d33
Yujin Shin <shinnew320@gmail.com>
2021-06-06 17:22:06 +0900

    fix: cors

--- ourney/src/main/java/com/lubycon/ourney/domains/user/controller/LoginController.java
+++ ourney/src/main/java/com/lubycon/ourney/domains/user/controller/LoginController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
-@CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @RestController
--- ourney/src/main/java/com/lubycon/ourney/domains/user/controller/UserController.java
+++ ourney/src/main/java/com/lubycon/ourney/domains/user/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
